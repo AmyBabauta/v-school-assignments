b3f0484bd4446d2a964bfe18574fbcf0a9829be5
Amy Babauta <amybabauta@gmail.com>
2022-06-21 23:28:06 -0700

    initial commit dj-js

---
+++ level-1/dj-js/index.js
@@ -0,0 +1,33 @@
+
+function mouseOver(event) {
+    event.target.style.backgroundColor = "blue";
+}
+
+function mouseDown(event) {
+    event.target.style.backgroundColor = "red";
+}
+
+function mouseUp(event) {
+    event.target.style.backgroundColor = "yellow";
+}
+
+function doubleClick(event) {
+    event.target.style.backgroundColor = "green";
+}
+
+function handleScroll(event) {
+    const square = document.getElementById("square");
+    square.style.backgroundColor = "orange";
+}
+
+document.getElementById("square").addEventListener("mouseover", (e) => mouseOver(e));
+
+document.getElementById("square").addEventListener("mousedown", (e) => mouseDown(e));
+
+document.getElementById("square").addEventListener("mouseup", (e) => mouseUp(e));
+
+document.getElementById("square").addEventListener("mouseup", (e) => mouseUp(e));
+
+document.getElementById("square").addEventListener("dblclick", (e) => doubleClick(e));
+
+document.getElementById("square").addEventListener("wheel", (e) => handleScroll(e));
