fdbadc985859b57bc5f85c9d5a20c5345d9d22d4
Amy Babauta <amybabauta@gmail.com>
2022-06-24 20:34:42 -0700

    subtitle attempt two-success

--- level-1/dom-olympics-start/index.js
+++ level-1/dom-olympics-start/index.js
@@ -3,6 +3,11 @@ h1.textContent = "JavaScript made this!";
 h1.className = "header";
 document.getElementById("header").appendChild(h1);
 
+const subTitle = document.createElement("subTitle");
+subTitle.innerHTML = "<span class='name'>Amy</span> wrote the JavaScript";
+subTitle.className = "header";
+document.getElementById("header").appendChild(subTitle)
+
 
 
 
