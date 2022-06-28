165a7a541af48f0e0e2b0596581977e3a49e6180
Amy Babauta <amybabauta@gmail.com>
2022-06-27 23:11:00 -0700

    messages change, working on button click

--- level-1/dom-olympics-start/index.js
+++ level-1/dom-olympics-start/index.js
@@ -8,6 +8,25 @@ subTitle.innerHTML = "<span class='name'>Amy</span> wrote the JavaScript";
 subTitle.className = "header";
 document.getElementById("header").appendChild(subTitle)
 
+const messages = document.getElementsByClassName("message");
+messages[0].textContent = "Good morning!";
+messages[1].textContent = "Hi there!";
+messages[2].textContent = "Did you sleep well?";
+messages[3].textContent = "Yes, thank you!";
+
+const inputBox = document.getElementById("input");
+inputBox.value = "Great! Goodbye!";
+
+// document.getElementsByClassName("message").addEventListener("click", handleButton);
+
+// function handleButton() {
+//     messages.length.textContent = "";
+   
+// }
+
+
+// document.getElementsByClassName
+// console.log(messages.length)
 
 
 
